d4c04b324f6bd83f45ae5be3dcb0e20c583846f4
KatsuKumi <bobbax4@live.fr>
2024-12-13 19:20:02 +0100

    Some changes to match current code

--- src/PhoenixWrapped/Messaging/Interaction/TargetEntity.cs
+++ src/PhoenixWrapped/Messaging/Interaction/TargetEntity.cs
@@ -1,24 +1,23 @@
 ﻿using Newtonsoft.Json;
 
-namespace PhoenixWrapped.Messaging.Interaction
+namespace PhoenixWrapped.Messaging.Interaction;
+
+public enum EntityType : byte
 {
-    public enum EntityType : byte
-    {
-        Player = 1,
-        Npc = 2,
-        Monster = 3
-    }
-    public class TargetEntity : Message
-    {
-        [JsonProperty("entity_type")]
-        public EntityType EntityType { get; set; }
+    Player = 1,
+    Npc = 2,
+    Monster = 3
+}
+public class TargetEntity : Message
+{
+    [JsonProperty("entity_type")]
+    public required EntityType EntityType { get; init; }
 
-        [JsonProperty("entity_id")]
-        public int EntityId { get; set; }
+    [JsonProperty("entity_id")]
+    public required int EntityId { get; init; }
 
-        public TargetEntity()
-        {
-            Type = MessageType.TargetEntity;
-        }
+    public TargetEntity()
+    {
+        Type = MessageType.TargetEntity;
     }
 }
